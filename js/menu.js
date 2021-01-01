29e7066b682fe0f5b498c212f2e9da5c7e3f5e64
Grygorii Tsaryk <artgrem@gmail.com>
2021-01-02 00:43:28 +0200

    added drop-down menu and section hero

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("click", () => {
+    console.log('CLICK');
+    console.log('MENU: ', mobileMenuRef);
+
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
