bb1698e48b3b4bfb44bdf838be3a7f06716da863
Grygorii Tsaryk <artgrem@gmail.com>
2021-01-09 15:12:20 +0200

    added fixed widht for top menu and menu-container style after 480px

--- js/menu.js
+++ js/menu.js
@@ -14,4 +14,15 @@
 
     mobileMenuRef.classList.toggle("is-open");
   });
+
+  document.body.addEventListener(
+    "touchmove",
+    function (e) {
+      // console.log(e.changedTouches[0].pageX); // показ коррдинат места прикосновения по X-у.
+      mobileMenuRef.classList.remove("is-open");
+      menuBtnRef.classList.remove("is-open");
+      menuBtnRef.setAttribute("aria-expanded", false);
+    },
+    false
+  );
 })();
